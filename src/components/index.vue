ceb93bdbffa886f7e9aafd11dbcb816ed9a62188
郑泽坚 <zhengzejian@xiaojienibuyaojiangzi.local>
2021-04-23 00:11:48 +0800

    init ref

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
-    <canvas></canvas>
+    <canvas ref="canvas"></canvas>
   </div>
 </template>
 
 <script>
-  import { onMounted } from 'vue'
+  import { onMounted, ref } from 'vue'
   export default {
     setup (props) {
-
+      const canvas = ref(null)
+      onMounted(() => {
+        console.log('canvas.value: ', canvas.value)
+      })
+      return {
+        canvas
+      }
     }
   }
 </script>
