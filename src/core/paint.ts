b56094968d3f6f11ac3de8eac58fc275bb3603ad
zhengzejian <458829@ky-tech.com.cn>
2021-04-29 19:27:28 +0800

    feat: draw header finish

--- src/core/paint.ts
+++ src/core/paint.ts
@@ -5,28 +5,37 @@ import config from '../config'
 
 export function paint(canvas: Ref<HTMLCanvasElement>, columns: Ref<Column[]>): void {
     paintHeader(canvas, columns)
-
 }
 
+// draw header
 function paintHeader(canvas: Ref<HTMLCanvasElement>, columns: Ref<Column[]>): void {
 
-    let { width, headerHeight, cellPaddingWidth, font } = config
+    let { width, headerHeight, headerColor, cellPaddingWidth, font, lineColor } = config
     let canvasCtx = state.canvasCtx as CanvasRenderingContext2D
     let { totalWidth, $columns } = state
     let canvasWidth = totalWidth > width ? width : totalWidth
 
     canvasCtx.beginPath()
+    canvasCtx.lineWidth = 1
+    canvasCtx.fillStyle = headerColor
+    canvasCtx.fillRect(0.5, 0.5, canvasWidth, headerHeight)
+
+    canvasCtx.strokeStyle = lineColor
     canvasCtx.strokeRect(0.5, 0.5, canvasWidth, headerHeight)
+
     $columns.forEach(column => {
         let { x, label } = column
         let textY = (13 + headerHeight) / 2
         canvasCtx.beginPath()
+        canvasCtx.lineWidth = 1
+        canvasCtx.strokeStyle = lineColor
         canvasCtx.moveTo(x, 0)
         canvasCtx.lineTo(x, headerHeight)
         canvasCtx.stroke()
 
         canvasCtx.beginPath()
         canvasCtx.font = `${font}`
+        canvasCtx.fillStyle = '#000'
         canvasCtx.fillText(label, x + cellPaddingWidth, textY)
     })
 }
